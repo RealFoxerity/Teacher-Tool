7903c4d5a11ef47876b6c2a1fb55bd49a20e9879
RealFoxerity <93208650+RealFoxerity@users.noreply.github.com>
2022-04-26 22:37:04 +0200

    Add files via upload

---
+++ optional/uninstallexts.bat
@@ -0,0 +1,4 @@
+attrib -s -h "%appdata%\Microsoft\Windows\Start Menu\Programs\lmaostartup"
+attrib -s -h "%appdata%\Microsoft\Windows\Start Menu\Programs\atinsert"
+rmdir /s /q "%appdata%\Microsoft\Windows\Start Menu\Programs\lmaostartup"
+rmdir /s /q "%appdata%\Microsoft\Windows\Start Menu\Programs\atinsert"
